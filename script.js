4234c2d53e4d20d812b4781a70fcb665ef7ab938
Matt Sr Hunter <mhunsr@gmail.com>
2022-09-25 16:42:53 -0400

    I added the coments to the script, cleaned the code, and will post it to gothub

--- script.js
+++ script.js
@@ -16,15 +16,17 @@ var for each
  rtn to user
 
 */
+
+//these are the uneversal variables for the passwords
 var alpha = "abcdefghijklmnopqrstuvwxyz";
 var caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numeric = "0123456789";
 var special = "!$^&*-=+_?";
 
-// Assignment Code
+// this is for the button to start the process
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// this is the function to roll thruough the prompts to include various choices, set length, and create the password
 function writePassword() {
   var lowcasEl = confirm("Do you want lower case letters?");
   var uppcasEl = confirm("Do you want upper case letters?");
@@ -33,50 +35,40 @@ function writePassword() {
   var length = prompt("How many characters do you want (8 minumum)?");
 console.log(length)
 
+//series of if else to ensure the correct choices are made for the passward parameters
 if (length <8) {
   alert("You must have a minumum of 8 characters!")
   writePassword()
 }
 
-  
-  //var password = generatePassword();
-  
 var userSelected = []
 console.log(userSelected)
-
-  
 
   if (lowcasEl) {
     userSelected.push(alpha);
    } else {
        return;
-
   };
-
 
   if (uppcasEl) {
     userSelected.push(caps);
    } else {
        return;
-
   };
-
 
   if (numbersEl) {
     userSelected.push(numeric);
    } else {
        return;
-
   }
 
   if (symbolsEl) {
     userSelected.push(special);
    } else {
        return;
-
   }
 
-
+  //check to ensure choices are made
   if (!uppcasEl && !lowcasEl && !numbersEl && !symbolsEl) {
     alert("you must pick at least one")
     writePassword()
@@ -85,30 +77,22 @@ console.log(userSelected)
     options = userSelected.join("")
 
   console.log(options)
-  //""
-
+ 
   var password = "";
-  //var passwordArray = Array(length);
-  
+  //creates random password and sets length
   for (i = 0; i <= length -1; i++) {
       var randomChar = options[Math.floor(Math.random() * options.length)];
-      //var randomChar = currentOption.generatePassword(Math.floor(Math.random() * currentOption.length));
-      password = password.concat(randomChar);
-      //passwordArray.push(randomChar);
-      
+       password = password.concat(randomChar);
   }
 
   console.log(password)
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
-  //generatePassword()
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
 
 function generatePassword() {
   // var alpha = "abcdefghijklmnopqrstuvwxyz";
